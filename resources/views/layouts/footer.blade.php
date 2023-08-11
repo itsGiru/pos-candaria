e7dbb46c02b23744218ed74bf0b9649eb2a90d9e
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-11 23:14:23 +0700

    fix double footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,6 @@
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      V 1.0.2a
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2023 <a href="https://github.com/ShirokamiRyzen">ShirokamiRyzen</a>.</strong> All rights reserved.
-  </footer>
-</div>
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> V 1.0.2a
+  </div>
+  <strong>Copyright &copy; 2023 <a href="https://github.com/ShirokamiRyzen">ShirokamiRyzen</a>.</strong> All rights reserved.
+</footer>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -55,20 +55,6 @@
         @include('layouts.app')
         @include('layouts.sidebar')
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>@yield('content-header')</h1>
-                        </div>
-                        <div class="col-sm-6 text-right">
-                            @yield('content-actions')
-                        </div><!-- /.col -->
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
 
             <!-- Main content -->
             <section class="content">
@@ -76,11 +62,6 @@
                 @include('layouts.alerts.error')
                 @yield('content')
             </section>
-
-        </div>
-        <!-- /.content-wrapper -->
-
-        @include('layouts.footer')
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
